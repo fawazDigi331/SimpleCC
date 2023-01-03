42a09a46f055e8568676ba938b906241353d9842
Fawaz Faiz <fawaz@digitalandcode.com>
2023-01-03 20:55:34 +0400

    New Test case added to check the pair url generates correctly from apiService

--- SimpleCurrencyTests/SimpleCurrencyTests.swift
+++ SimpleCurrencyTests/SimpleCurrencyTests.swift
@@ -21,11 +21,21 @@ class SimpleCurrencyTests: XCTestCase {
     func testStorage() throws {
         var currencies: Currency?
         let recievedData = Storage.retrieve(storageKey.pairCurrencies, from: .documents, as: Currency.self)
-        
         currencies = recievedData
         XCTAssertEqual(currencies?.result, "success")
     }
-
+    
+    // test pair url
+    let apiService: APIServiceProtocol = APIService()
+    var currencyInValue = "GBP"
+    var currencyOutValue = "EUR"
+    var currencyValue = "1"
+    func testUrl() throws{
+        let urlString = apiService.pairConversionUrl(currencyInCode: self.currencyInValue, currencyOutCode: self.currencyOutValue, amount: self.currencyValue)
+    let sampleUrl = "https://v6.exchangerate-api.com/v6/7912b8457e7c54b435870faa/pair/GBP/EUR/1"
+    XCTAssertEqual(urlString, sampleUrl)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
